55ccaec4e6182eda6ba00fb6a9d2a636d86e65e8
Albert Fiati-Kumasenu <awkfiati@gmail.com>
2013-06-19 18:19:15 +0000

    After all test run successfully

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -57,14 +57,41 @@ public class BasicTest extends UnitTest {
     	User bob = new User("bob@gmail.com","Bob Minta","secret").save();
     	Post bobPost = new Post(bob,"My Second Post","I am proud of myself").save();
     	new Comment(bobPost,"Albert Fiati","You are doing a great job").save();
+    	new Comment(bobPost,"Julliet Joyce","Interesting post").save();
     	
     	//tests
-    	assertEquals(1,Comment.count());
+    	assertEquals(2,Comment.count());
     	assertNotNull(Comment.find("byAuthor","Albert Fiati"));
     	List<Comment> bobPostComments = Comment.find("byPost", bobPost).fetch();
     	Comment firstComment = bobPostComments.get(0);
+    	Comment secondComment = bobPostComments.get(1);
     	assertEquals("You are doing a great job", firstComment.content);
-    	assertEquals("Albert Fiati",firstComment.author);
+    	assertEquals("Interesting post",secondComment.content);
     	//assertNotNull(firstComment.postedAt);
+    }
+    
+    @Test
+    public void useTheCommentsRelation(){
+    	User bob = new User("bob@gmail.com","Bob Minta","secret").save();
+    	Post bobPost = new Post(bob,"Ghana Leaks","Ghana dey Bii K3k3").save();
+    	
+    	bobPost.addComment("Helen Odame", "This is great news");
+    	bobPost.addComment("Mansah Ghana","Which part of Ghana dey biiii?");
+    	
+    	assertEquals(1,Post.count());
+    	assertEquals(1,User.count());
+    	assertEquals(2, Comment.count());
+    	
+    	bobPost = Post.find("byAuthor", bob).first();
+    	assertNotNull(bobPost);
+    	assertEquals(2, bobPost.comments.size());
+    	assertEquals("Helen Odame", bobPost.comments.get(0).author);
+    	
+    	bobPost.delete();
+    	
+    	assertEquals(0, Post.count());
+    	assertEquals(1,User.count());
+    	assertEquals(0, Comment.count());
+    	
     }
 }
